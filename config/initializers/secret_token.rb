038d5e044a4dbae90401615528f583dbfe1c4052
Forys <marcin.forysinski@gmail.com>
2014-03-12 17:28:12 +0100

    Second Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenealogyTree::Application.config.secret_token = '96d2546561a49223de345399848671746177998c5fa5e731892ad652691216cc203700a6382f3577740e95b991661111a28d165e93ec92ee05fd5fb109838ef1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GenealogyTree::Application.config.session_store :cookie_store, key: '_GenealogyTree_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GenealogyTree::Application.config.session_store :active_record_store
